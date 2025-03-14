a76b36f9f6033cffe3eb8afe283420a60ed7807d
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-03-14 22:41:18 +0700

    add: menyelesaikan exam 3

--- bin/exam3/exam3.dart
+++ bin/exam3/exam3.dart
@@ -1,7 +1,7 @@
 Set uniqueElement(List<int> myList) {
   // TODO 1
 
-  return {};
+  return {...myList};
 
   // End of TODO 1
 }
@@ -9,7 +9,14 @@ Set uniqueElement(List<int> myList) {
 Map<String, String> buildFutsalPlayersMap() {
   // TODO 2
 
-  return {};
+  var futsalPlayer = {
+    'Goalkeeper': 'Andri',
+    'Anchor': 'Irfan',
+    'Pivot': 'Fikri',
+    'Right Flank': 'Aldi',
+    'Left Flank': 'Hafid',
+  };
+  return futsalPlayer;
 
   // End of TODO 2
 }
@@ -18,6 +25,8 @@ Map<String, String> updatePivotPlayer() {
   final futsalPlayers = buildFutsalPlayersMap();
 
   // TODO 3
+
+  futsalPlayers['Pivot'] = 'Fajar';
 
   // End of TODO 3
 
