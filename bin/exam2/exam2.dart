cdd3f402ea7704473e332fc28f41812afb8a6155
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-03-14 22:35:03 +0700

    add: menyelesaikan exam 2

--- bin/exam2/exam2.dart
+++ bin/exam2/exam2.dart
@@ -1,7 +1,11 @@
 dynamic oddOrEven(int number) {
   // TODO 1
 
-  return null;
+  if (number % 2 == 0){
+    return "Genap";
+  }else{
+    return "Ganjil";
+  }
 
   // End of TODO 1
 }
@@ -10,7 +14,15 @@ dynamic createListOneToX(int x) {
   final List<int> list = [];
 
   // TODO 2
-
+  if (x < 1){
+    return [];
+  }else{
+    List<int> deret = [];
+    for (int i = 1; i <= x; i++){
+      deret.add(i);
+    }
+    return deret;
+  }
   // End of TODO 2
 
   return list;
@@ -20,7 +32,15 @@ String getStars(int n) {
   var result = '';
 
   // TODO 3
-
+  while (n >= 1){
+    int i = n;
+    while (i>=1){
+      result = result + '*';
+      i--;
+    }
+    result = result + '\n';
+    n--;
+  }
   // End of TODO 3
 
   return result;
